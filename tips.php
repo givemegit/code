ed5819bda8d7cff3a3b00bcaea9d556ac8da1a4d
givemegit <givememail25@gmail.com>
2018-12-20 23:31:38 +0400

    tip added

--- tips.php
+++ tips.php
@@ -1,2 +1,11 @@
 <!-- How to add alias to cmder  -->
 <!-- https://github.com/cmderdev/cmder/issues/421 -->
+
+
+remove git creditils
+error
+remote: Permission to givemegit/code.git denied to vaskita101.
+fatal: unable to access 'https://github.com/givemegit/code.git/':
+
+fix link
+https://stackoverflow.com/questions/15381198/remove-credentials-from-git
